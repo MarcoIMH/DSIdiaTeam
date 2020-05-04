30279842a8725b40c1260831b42eb8ea10366e98
Javier Cruz López de Ochoa <javcruz@ucm.es>
2020-05-04 11:49:06 +0200

    Arreglos

--- FinalProyect/FinalProyect/start_game.xaml.cs
+++ FinalProyect/FinalProyect/start_game.xaml.cs
@@ -29,6 +29,14 @@ namespace FinalProyect
 
         private void Grid_SizeChanged(object sender, SizeChangedEventArgs e)
         {
+            exit.Width = grid_.ColumnDefinitions.ElementAt(0).ActualWidth * 2 / 3;
+            exit.Height = grid_.RowDefinitions.ElementAt(0).ActualHeight;
+
+            user1.Width = user1.Height = grid_.RowDefinitions.ElementAt(1).ActualHeight;
+            user2.Width = user2.Height = grid_.RowDefinitions.ElementAt(1).ActualHeight;
+            user3.Width = user3.Height = grid_.RowDefinitions.ElementAt(1).ActualHeight;
+            user4.Width = user4.Height = grid_.RowDefinitions.ElementAt(1).ActualHeight;
+
             chat.MaxHeight = grid_.RowDefinitions.ElementAt(3).ActualHeight * 2 / 3;
         }
     }
