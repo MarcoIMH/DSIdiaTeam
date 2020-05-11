ccf1747682e660d0ab674a1f62eda895e57f5b3e
Will7461 <43720307+Will7461@users.noreply.github.com>
2020-05-11 19:44:32 +0200

    Pagina 6 adaptative size

--- FinalProyect/FinalProyect/Page6.xaml.cs
+++ FinalProyect/FinalProyect/Page6.xaml.cs
@@ -71,10 +71,42 @@ namespace FinalProyect
 
                 dispatcherTimer.Stop();
                 //IsEnabled should now be false after calling stop
-
+                PlayBtn.IsEnabled = true;
                 span = stopTime - startTime;
 
             }
+        }
+
+        private void gridSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            policeRect.Width = ((Frame)Window.Current.Content).ActualWidth * 0.13;
+            policeText.FontSize = (policeRect.Width / 10) + 12;
+            policeList.Width = policeRect.Width;
+            policeList.Height = ((Frame)Window.Current.Content).ActualHeight * 0.5;
+
+            robberRect.Width = ((Frame)Window.Current.Content).ActualWidth * 0.13;
+            robberText.FontSize = (robberRect.Width / 10) + 12;
+            robberList.Width = policeRect.Width;
+            robberList.Height = policeList.Height;
+
+            Map.Width = ((Frame)Window.Current.Content).ActualWidth * 0.4;
+            Map.Height = ((Frame)Window.Current.Content).ActualHeight * 0.4;
+            MapName.FontSize = Map.Width / 10 + 12;
+            EventName.FontSize = MapName.FontSize - 20;
+            PlayBtn.Width = Map.Width * 0.3;
+            PlayBtn.Height = ((Frame)Window.Current.Content).ActualHeight * 0.1;
+            textoJugar.FontSize = PlayBtn.Width / 4;
+
+            Map.Margin = new Thickness { Top = (MapName.FontSize+20) + (EventName.FontSize+20)};
+            EventName.Margin = new Thickness { Top = (MapName.FontSize)};
+            PoliceIcon.Margin = new Thickness{Left = policeList.Width + 20};
+            RobberIcon.Margin = new Thickness{Right = robberList.Width + 20};
+            PlayBtn.Margin = new Thickness { Top = Map.Margin.Top + Map.Height};
+            LoadingText.Margin = new Thickness { Bottom = LoadingBar.Height/3 - 15 };
+
+            LoadingBar.Width = ((Frame)Window.Current.Content).ActualWidth * 0.8;
+            LoadingText.FontSize = MapName.FontSize;
+
         }
     }
 }
