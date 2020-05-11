3d044d0125cbe811dad0af0af81fa1e01dc2b879
Will7461 <43720307+Will7461@users.noreply.github.com>
2020-05-11 15:38:19 +0200

    timer

--- FinalProyect/FinalProyect/Page6.xaml.cs
+++ FinalProyect/FinalProyect/Page6.xaml.cs
@@ -22,6 +22,12 @@ namespace FinalProyect
     /// </summary>
     public sealed partial class Page6 : Page
     {
+        DispatcherTimer dispatcherTimer;
+        DateTimeOffset startTime;
+        DateTimeOffset lastTime;
+        DateTimeOffset stopTime;
+        int timesTicked = 1;
+        int timesToTick = 100;
         public Page6()
         {
             this.InitializeComponent();
@@ -30,6 +36,45 @@ namespace FinalProyect
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(start_game));
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            DispatcherTimerSetup();
+        }
+        public void DispatcherTimerSetup()
+        {
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 25);
+            //IsEnabled defaults to false
+
+            startTime = DateTimeOffset.Now;
+            lastTime = startTime;
+
+            dispatcherTimer.Start();
+            //IsEnabled should now be true after calling start
+
+        }
+
+        void dispatcherTimer_Tick(object sender, object e)
+        {
+            DateTimeOffset time = DateTimeOffset.Now;
+            TimeSpan span = time - lastTime;
+            lastTime = time;
+            //Time since last tick should be very very close to Interval
+            LoadingBar.Value += 1;
+            timesTicked++;
+            if (timesTicked > timesToTick)
+            {
+                stopTime = time;
+
+                dispatcherTimer.Stop();
+                //IsEnabled should now be false after calling stop
+
+                span = stopTime - startTime;
+
+            }
         }
     }
 }
